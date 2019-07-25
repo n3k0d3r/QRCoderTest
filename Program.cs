b4e46032c2ed060f82c52996acd3d2ebaa952f02
Sebastian Perry <sperry@biggby.com>
2019-07-25 09:40:38 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+
+namespace QRCoderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            Console.Write("QR Code Input: ");
+            string input = Console.ReadLine();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            qrCodeImage.Save("./qr-code-" + DateTime.Now.ToString("yyyy-MM-dd-hhmmss") + ".png", ImageFormat.Png);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
