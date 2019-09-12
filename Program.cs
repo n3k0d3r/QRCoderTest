252de26c985faf350a9093ebf0fe8fe9a32d07fb
Sebastian Perry <sperry@biggby.com>
2019-09-12 15:16:04 -0400

    Added XMLWriter test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.Drawing.Imaging;
 using QRCoder;
+using System.IO;
+using System.ComponentModel;
+using System.Xml.Linq;
+using System.Xml;
 
 namespace QRCoderTest
 {
@@ -13,6 +17,7 @@ namespace QRCoderTest
     {
         static void Main(string[] args)
         {
+            XmlWriter xmlWriter = XmlWriter.Create("./test.xml", null);
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             Console.Write("QR Code Input: ");
             string input = Console.ReadLine();
@@ -20,6 +25,13 @@ namespace QRCoderTest
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap qrCodeImage = qrCode.GetGraphic(20);
             qrCodeImage.Save("./qr-code-" + DateTime.Now.ToString("yyyy-MM-dd-hhmmss") + ".png", ImageFormat.Png);
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(Bitmap));
+            XElement img = new XElement("image", Convert.ToBase64String((byte[])converter.ConvertTo(qrCodeImage, typeof(byte[]))));
+            xmlWriter.WriteStartDocument();
+            xmlWriter.WriteStartElement("qrcodertest");
+            xmlWriter.WriteString(img.Value);
+            xmlWriter.WriteEndElement();
+            xmlWriter.Close();
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey(true);
         }
